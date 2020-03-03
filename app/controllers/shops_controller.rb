bec14e93a37064c6a4c173ca5a42450809656d7c
Olivier Rits <olivier.rits@imec.be>
2020-03-03 17:10:45 +0100

    resolves issues in controllers and routes.rb

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,6 @@
 class ShopsController < ApplicationController
 
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def index
     @shops = Shop.all
