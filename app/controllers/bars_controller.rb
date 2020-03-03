82542ad2eaf663c35dc41efd841242f546738415
Thibaut Moreau <thibaut.moreau.massart@gmail.com>
2020-03-03 15:26:14 +0100

    created the bars controller

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,8 +2,11 @@ class BarsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def index
+    @bars = Bars.all
   end
 
   def show
+    @bar = Bars.find(params[:id])
   end
+
 end
