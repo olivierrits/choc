2ee62b539d431d19de8935f6396ccd043c197f59
Veronica Lopez <v.lopezmontoro@gmail.com>
2020-03-13 13:57:58 +0100

    Add status

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -14,4 +14,41 @@ class Shop < ApplicationRecord
   def full_address
     address.to_s
   end
+
+  def status
+    @status ||= get_status
+  end
+
+  def status_to_s
+    return "Opening soon" if status == "opening_soon"
+    return "Open" if status == "open"
+    return "Closing soon" if status == "closing_soon"
+    return "Closed" if status == "closed"
+  end
+
+  def status_class
+    return "fas fa-times-circle icon-orange" if status == "opening_soon"
+    return "fas fa-check-circle icon-green" if status == "open"
+    return "fas fa-check-circle icon-orange" if status == "closing_soon"
+    return "fas fa-times-circle icon-red" if status == "closed"
+  end
+
+  def get_status(time=DateTime.now)
+    time = time.change(day: 1, month: 1, year: 2000).asctime.in_time_zone("UTC")
+    tt = today_open_times(time)
+    return "opening_soon" if today_open_times.where("opening_hour < :min AND opening_hour > :max", min: time + 30.minutes, max: time).any?
+    return "open" if today_open_times.where("opening_hour < :min AND closing_hour > :max", min: time, max: time + 30.minutes).any?
+    return "closing_soon" if today_open_times.where("closing_hour < :min AND closing_hour > :max", min: time + 30.minutes, max: time).any?
+    return "closed"
+  end
+
+  def today_open_times(time=DateTime.now)
+    time = time.change(day: 1, month: 1, year: 2000).asctime.in_time_zone("UTC")
+    today_open_times = opening_times.where(day: weekday(time), open: true)
+  end
+
+  def weekday(day)
+    result = (day.wday + 6) % 7
+    result.zero? ? 7 : result
+  end
 end
